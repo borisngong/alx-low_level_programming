e4700155bc4e752c49b9086d4e1117405e91ee35
borisngong <borisngong@gmail.com>
2023-03-21 09:27:46 -0700

    Absolute Number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_abs - computes absolute value of an interger
+  *@j: input number of an interger
+  *Return: Absolute number
+  */
+int _abs(int j)
+{
+	if (j > 0)
+	{
+		return (j);
+	}
+	else
+	{
+		return (j = -1);
+	}
+	_putchar('\n');
+}

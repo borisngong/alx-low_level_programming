c88766b2b5d1143d187ea426b3ecd8ca5220500f
borisngong <borisngong@gmail.com>
2023-03-29 18:43:06 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *_strncat - Write a function that concatenates two strings.
+  *@dest: copying to
+  *@src: copying from
+  *Return: void
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d;
+	int s;
+
+	d = 0;
+	while (dest[d] != '\0')
+	{
+		d++;
+	}
+	s = 0;
+	while (s < n && src[s] != '\0')
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+	dest[d] = '\0';
+	return (dest);
+}
+

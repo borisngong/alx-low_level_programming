09b88f3de52d14e9fcb8f219a5e8698e282b6090
borisngong <borisngong@gmail.com>
2023-09-01 14:09:16 -0700

    A programm that prints number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A programm that print all received arguments passed to it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (Always Successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; 
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+

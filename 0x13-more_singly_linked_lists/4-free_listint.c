9068e1c10621ca27837777f6a7a5b9b56afe6fcd
borisngong <borisngong@gmail.com>
2023-09-26 08:06:59 -0700

    a function that frees a listint_t list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-  * free_lintint -  a function that frees a listint_t list
+  * free_listint - a function that frees a listint_t list
   * @head: refence or pointer to list first node
   * Return: (void)
   */

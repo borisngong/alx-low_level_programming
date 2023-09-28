d321a8afd9087dcda6e8a5e12663672e592e8a93
borisngong <borisngong@gmail.com>
2023-09-27 17:39:27 -0700

    a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,27 +1,64 @@
 #include "lists.h"
 /**
- * print_listint_safe - prints elements of  a listint_t linked list
- * safe even if it contains a loop
- * @head: pointer to the head of the linked list
- * Return: number of nodes in the list, exit 98 on failure
- */
-size_t print_listint_safe(const listint_t *head)
+  * free_listp - responsible for freeing the memory allocated
+  * for a linked list
+  * pointer to the head of the linked list
+  * Return: (void)
+  */
+void free_listp(listp_t **head)
 {
-	const listint_t *current_node = head;
-	size_t aggregate = 0;
+	listp_t *temporal_node;
+	listp_t *current_node;
 
-	while (current_node != NULL)
+	if (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)current_node, current_node->n);
-		aggregate++;
-
-		if (current_node <= current_node->next)
+		current_node = *head;
+		while ((temporal_node = current_node) != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)current_node->next, current_node->next->n);
-			exit(98);
+			current_node = current_node->next;
+			free(temporal_node);
 		}
-		current_node = current_node->next;
+		*head = NULL;
 	}
 
-	return (aggregate);
+}
+/**
+  * print_listint_safe - prints a linked list safe with loop.
+  * @head: pointer to head of list
+  * Return: number of nodes on success, 98 exit on failure
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	listp_t *headptr, *node_new, *node_add;
+	size_t node_aggregate = 0;
+
+	headptr = NULL;
+
+	while (head != NULL)
+	{
+		node_new = malloc(sizeof(listp_t));
+		if (node_new == NULL)
+			exit(98);
+		node_new->p = (void *)head;
+		node_new->next = headptr;
+		headptr = node_new;
+
+		node_add = headptr;
+
+		while (node_add->next != NULL)
+		{
+			node_add = node_add->next;
+			if (head == node_add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&headptr);
+				return (node_aggregate);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		node_aggregate++;
+	}
+	free_listp(&headptr);
+	return (node_aggregate);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+  * struct listp_s - linked list
+  * @p: node pointers
+  * @next: links the next noe
+  */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -32,6 +43,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
-int _putchar(char c);
 
 #endif /* _LISTS_H_ */

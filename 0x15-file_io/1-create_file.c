7825d728dc9695c240e2252c446358d5f99af19e
borisngong <borisngong@gmail.com>
2023-10-02 16:18:05 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,8 +28,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		text_content = "";
-	for (l_write = 0; text_content[l_longii];)
+	if (text_content != NULL)
+	{
+		for (l_write = 0; text_content[l_longii];)
 		l_longii++;
+	}
 	l_write = write(fd, text_content, l_longii);
 	if (l_write == -1)
 		return (-1);

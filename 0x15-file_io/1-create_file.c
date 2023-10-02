a9743858a495f114bcb1fa5acdcc6e48b0cb2a96
borisngong <borisngong@gmail.com>
2023-10-02 16:56:27 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,20 +15,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (l_longii = 0; text_content[l_longii];)
-			l_longii++;
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	l_write = write(fd, text_content, l_longii);
-
-	if (fd == -1 || l_write == -1)
+	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
+	for (l_longii = 0; text_content[l_longii]; l_longii++)
+		;
+	l_write = write(fd, text_content, l_longii);
+
+	if (l_write == -1)
+		return (-1);
 	close(fd);
 
 	return (1);
 }
-

43cd67d67b33e27e6e0737eacc2176686ef27a90
borisngong <borisngong@gmail.com>
2023-10-03 17:15:15 -0700

    program that copies the content of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp source_fd destination_fd\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(98);
 	}
 

048c579e88169d3b4dd180a0dbbb981016d9b5a2
borisngong <borisngong@gmail>
2023-12-07 08:07:30 -0800

    A function that prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - Doubly linked list node structure
+ * @n: Integer stored in the node
+ * @prev: represents a pointer to the previous element of the list
+ * @next: represents a pointer to the next element of the list
+ *
+ * Description: Doubly linked list node structure for dlistint_t
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *next;
+	struct dlistint_s *prev;
+} dlistint_t;
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
+
